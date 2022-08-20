150848685f870794b83d5a046a427ab2d00b04d8
Ecumelos <wgonzalezbog@gmail.com>
2022-08-20 15:53:29 -0500

    Este es mi primer commit usando GIT

---
+++ src/main/java/Paquete1GITHUB/Clase1probarGITHUB.java
@@ -0,0 +1,7 @@
+package Paquete1GITHUB;
+
+public class Clase1probarGITHUB {
+    public static void main(String[] args) {
+        System.out.println("probando GITHUB para hacer los commit");
+    }
+}
