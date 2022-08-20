cb68684c7ae45e073e35eb712748ed0aaa60e40d
Ecumelos <wgonzalezbog@gmail.com>
2022-08-20 15:55:08 -0500

    Este es el segundo commit

--- src/main/java/Paquete1GITHUB/Clase1probarGITHUB.java
+++ src/main/java/Paquete1GITHUB/Clase1probarGITHUB.java
@@ -3,5 +3,6 @@ package Paquete1GITHUB;
 public class Clase1probarGITHUB {
     public static void main(String[] args) {
         System.out.println("probando GITHUB para hacer los commit");
+        System.out.println("segunda modificacion");
     }
 }
